21ec714129a6192285e50d5a6e67a3d409180234
dessant <git@rmn.space>
2019-05-23 00:06:22 +0300

    fix: wrap rename expression in single quotes

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,7 +5,7 @@ yarn
 
 if [ -n "${TRAVIS_TAG}" ]; then
   yarn build:prod:zip:all
-  rename "s|artifacts/(.*)/(.*)\.zip$|artifacts/$1/$2-$1\.zip|" artifacts/*/*.zip
+  rename 's|artifacts/(.*)/(.*)\.zip$|artifacts/$1/$2-$1\.zip|' artifacts/*/*.zip
 else
   yarn build:all
 fi
