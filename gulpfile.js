2190a2dc0ed04603d01a706c4837e515190d9d66
dessant <git@armin.dev>
2020-09-23 23:49:26 +0300

    fix: update dependencies and rework build script

--- gulpfile.js
+++ gulpfile.js
@@ -2,80 +2,84 @@ const path = require('path');
 const {exec} = require('child_process');
 const {lstatSync, readdirSync, readFileSync, writeFileSync} = require('fs');
 
-const {ensureDirSync} = require('fs-extra');
-const gulp = require('gulp');
+const {series, parallel, src, dest} = require('gulp');
 const gulpif = require('gulp-if');
-const del = require('del');
 const jsonMerge = require('gulp-merge-json');
 const jsonmin = require('gulp-jsonmin');
-const svg2png = require('svg2png');
 const imagemin = require('gulp-imagemin');
+const del = require('del');
+const {ensureDirSync} = require('fs-extra');
+const sharp = require('sharp');
 
 const targetEnv = process.env.TARGET_ENV || 'firefox';
 const isProduction = process.env.NODE_ENV === 'production';
 const distDir = path.join('dist', targetEnv);
 
-gulp.task('clean', function() {
+function clean() {
   return del([distDir]);
-});
+}
 
-gulp.task('icons', async function(done) {
-  ensureDirSync(`${distDir}/src/icons/app`);
-  const iconSvg = readFileSync('src/icons/app/icon.svg');
+async function images(done) {
+  ensureDirSync(path.join(distDir, 'src/icons/app'));
+  const appIconSvg = readFileSync('src/icons/app/icon.svg');
   const appIconSizes = [16, 19, 24, 32, 38, 48, 64, 96, 128];
   for (const size of appIconSizes) {
-    const pngBuffer = await svg2png(iconSvg, {width: size, height: size});
-    writeFileSync(`${distDir}/src/icons/app/icon-${size}.png`, pngBuffer);
+    await sharp(appIconSvg, {density: (72 * size) / 24})
+      .resize(size)
+      .toFile(path.join(distDir, `src/icons/app/icon-${size}.png`));
   }
-
-  if (isProduction) {
-    gulp
-      .src(`${distDir}/src/icons/**/*.png`, {base: '.'})
-      .pipe(imagemin())
-      .pipe(gulp.dest('.'));
+  // Chrome Web Store does not correctly display optimized icons
+  if (isProduction && targetEnv !== 'chrome') {
+    await new Promise(resolve => {
+      src(path.join(distDir, 'src/icons/app/*.png'), {base: '.'})
+        .pipe(imagemin())
+        .pipe(dest('.'))
+        .on('error', done)
+        .on('finish', resolve);
+    });
   }
-  done();
-});
+}
 
-gulp.task('locale', function(done) {
+async function locale(done) {
   const localesRootDir = path.join(__dirname, 'src/_locales');
-  const localeDirs = readdirSync(localesRootDir).filter(function(file) {
+  const localeDirs = readdirSync(localesRootDir).filter(function (file) {
     return lstatSync(path.join(localesRootDir, file)).isDirectory();
   });
-  localeDirs.forEach(function(localeDir) {
+  for (const localeDir of localeDirs) {
     const localePath = path.join(localesRootDir, localeDir);
-    gulp
-      .src(
+    await new Promise(resolve => {
+      src(
         [
           path.join(localePath, 'messages.json'),
           path.join(localePath, `messages-${targetEnv}.json`)
         ],
         {allowEmpty: true}
       )
-      .pipe(
-        jsonMerge({
-          fileName: 'messages.json',
-          edit: (parsedJson, file) => {
-            if (isProduction) {
-              for (let [key, value] of Object.entries(parsedJson)) {
-                if (value.hasOwnProperty('description')) {
-                  delete parsedJson[key].description;
+        .pipe(
+          jsonMerge({
+            fileName: 'messages.json',
+            edit: (parsedJson, file) => {
+              if (isProduction) {
+                for (let [key, value] of Object.entries(parsedJson)) {
+                  if (value.hasOwnProperty('description')) {
+                    delete parsedJson[key].description;
+                  }
                 }
               }
+              return parsedJson;
             }
-            return parsedJson;
-          }
-        })
-      )
-      .pipe(gulpif(isProduction, jsonmin()))
-      .pipe(gulp.dest(path.join(distDir, '_locales', localeDir)));
-  });
-  done();
-});
+          })
+        )
+        .pipe(gulpif(isProduction, jsonmin()))
+        .pipe(dest(path.join(distDir, '_locales', localeDir)))
+        .on('error', done)
+        .on('finish', resolve);
+    });
+  }
+}
 
-gulp.task('manifest', function(done) {
-  gulp
-    .src('src/manifest.json')
+function manifest() {
+  return src('src/manifest.json')
     .pipe(
       jsonMerge({
         fileName: 'manifest.json',
@@ -99,11 +103,10 @@ gulp.task('manifest', function(done) {
       })
     )
     .pipe(gulpif(isProduction, jsonmin()))
-    .pipe(gulp.dest(distDir));
-  done();
-});
+    .pipe(dest(distDir));
+}
 
-gulp.task('license', function(done) {
+function license() {
   let year = '2017';
   const currentYear = new Date().getFullYear().toString();
   if (year !== currentYear) {
@@ -117,25 +120,20 @@ This software is released under the terms of the GNU General Public License v3.0
 See the LICENSE file for further information.
 `;
 
-  writeFileSync(`${distDir}/NOTICE`, notice);
-  gulp.src(['LICENSE']).pipe(gulp.dest(distDir));
-  done();
-});
+  writeFileSync(path.join(distDir, 'NOTICE'), notice);
+  return src(['LICENSE']).pipe(dest(distDir));
+}
 
-gulp.task(
-  'build',
-  gulp.series('clean', gulp.parallel('icons', 'locale', 'manifest', 'license'))
-);
-
-gulp.task('zip', function(done) {
+function zip(done) {
   exec(
-    `web-ext build -s dist/${targetEnv} -a artifacts/${targetEnv} --overwrite-dest`,
-    function(err, stdout, stderr) {
+    `web-ext build -s dist/${targetEnv} -a artifacts/${targetEnv} -n '{name}-{version}-${targetEnv}.zip' --overwrite-dest`,
+    function (err, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
       done(err);
     }
   );
-});
+}
 
-gulp.task('default', gulp.series('build'));
+exports.build = series(clean, parallel(images, locale, manifest, license));
+exports.zip = zip;
